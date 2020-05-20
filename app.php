d21339ae3a1d3b0da0f89d6b037ffe6f2f857d24
eaktamp <eaktamp@gmail.com>
2020-05-20 14:22:04 +0700

    clone

---
+++ app.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+include"config/pg_con.class.php";
+include"config/func.class.php";
+
+//$hn =  $_GET[''];
+$hn =  "000191812";
+
+
+?>
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.20/css/uikit.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/radio.css">
+</head>
+<?php
+$sql = " SELECT  o.nextdate AS dateapp ,C.NAME AS clinic
+,o.hn
+,CAST ( concat ( P.pname, P.fname, '  ', P.lname ) AS VARCHAR ( 250 )) AS patientname
+,d.NAME AS doctor 
+,CONCAT(pp.pttype,' ',pp.name) as insptty
+FROM oapp o
+LEFT JOIN vn_stat v ON v.vn = o.vn
+LEFT  JOIN patient P ON P.hn = o.hn 
+LEFT  JOIN clinic C ON C.clinic = o.clinic
+LEFT  JOIN doctor d ON d.code = o.doctor
+LEFT  JOIN kskdepartment K ON K.depcode = o.depcode
+LEFT JOIN pttype as pp ON pp.pttype = v.pttype  
+WHERE   1 = 1
+AND p.hn = '$hn'
+AND o.nextdate > CURRENT_DATE
+AND (( o.oapp_status_id < 4 ) OR o.oapp_status_id IS NULL ) 
+ORDER BY    o.nextdate";
+$result = pg_query($conn, $sql);
+?>
+<body>
+    <div class="uk-container uk-padding">
+        <h1>เลือกรายการนัด</h1>
+        <hr>
+        <form id="" class=""  autocomplete="">
+            <?php
+            $rw=0;
+            while($row_result = pg_fetch_array($result)) 
+            { 
+                $rw++;
+                ?>
+                <div class="">
+                    <div>
+                      <input type="radio" id="<?=$rw;?>" name="select" value="2">
+                      <label for="<?=$rw;?>">
+                        <h2 class="hh2"><?php echo thaiDateFULL($row_result['dateapp']); ?></h2>
+                        <p class="p1"><?php echo $row_result['clinic']; ?></p>
+                        <p class="p2"><?php echo $row_result['doctor'];?></p>
+                    </label>
+                </div>
+            </div>
+            <br>
+        <?php
+        }
+        ?>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kanit">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.20/css/uikit.css">
+    <link rel="stylesheet" href="jquery.Thailand.js/dist/jquery.Thailand.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <style>
+
+    </style>
+
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-33058582-1', 'auto', {
+            'name': 'Main'
+        });
+        ga('Main.send', 'event', 'jquery.Thailand.js', 'GitHub', 'Visit');
+    </script>
+
+</head>
+<body>
+    <div class="uk-container uk-padding">
+        <h1>ข้อมูลxxxxxxxxxx</h1>
+        <div id="loader">
+            <div uk-spinner></div> รอสักครู่ กำลังโหลดฐานข้อมูล...
+        </div>
+        <form id="demo1" class="demo" style="display:none;" autocomplete="off" uk-grid >
+
+            <div class="uk-width-1-2@m">
+                <label class="h2">ชื่อ</label>
+                <input name="fname" class="uk-input uk-width-1-1" type="text">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">นามสกุล</label>
+                <input name="lname" class="uk-input uk-width-1-1" type="text">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">เบอร์โทรศัพท์</label>
+                <input name="phone" class="uk-input uk-width-1-1" type="text">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">LINE ID:</label>
+                <input name="lineid" class="uk-input uk-width-1-1" type="text">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">เลขที่</label>
+                <input name="adddess" class="uk-input uk-width-1-1" type="text">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">หมู่</label>
+                <input name="moo" class="uk-input uk-width-1-1" type="text">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">ตำบล / แขวง</label>
+                <input name="district" class="uk-input uk-width-1-1" type="text" value="25130">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">อำเภอ / เขต</label>
+                <input name="amphoe" class="uk-input uk-width-1-1" type="text" value="25130">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">จังหวัด</label>
+                <input name="province" class="uk-input uk-width-1-1" type="text" value="25130">
+            </div>
+            <div class="uk-width-1-2@m">
+                <label class="h2">รหัสไปรษณีย์</label>
+                <input name="zipcode" class="uk-input uk-width-1-1" type="text" value="25130">
+            </div>
+            <div class="uk-width-1-2@m">
+             <button class="button" style="vertical-align:middle"><span>ยืนยันข้อมูล </span></button>
+         </div>
+     </form>
+ </div>
+ <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script> 
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.20/js/uikit.min.js"></script>
+ <script type="text/javascript" src="jquery.Thailand.js/dependencies/zip.js/zip.js"></script>
+ <script type="text/javascript" src="jquery.Thailand.js/dependencies/JQL.min.js"></script>
+ <script type="text/javascript" src="jquery.Thailand.js/dependencies/typeahead.bundle.js"></script>
+ <script type="text/javascript" src="jquery.Thailand.js/dist/jquery.Thailand.min.js"></script>
+ <script type="text/javascript">
+    $.Thailand({
+        database: 'jquery.Thailand.js/database/db.json', 
+        $district: $('#demo1 [name="district"]'),
+        $amphoe:   $('#demo1 [name="amphoe"]'),
+        $province: $('#demo1 [name="province"]'),
+        $zipcode:  $('#demo1 [name="zipcode"]'),
+        onDataFill: function(data){
+            console.info('Data Filled', data);
+        },
+        onLoad: function(){
+            console.info('Autocomplete is ready!');
+            $('#loader, .demo').toggle();
+        }
+    });
+    $('#demo1 [name="district"]').change(function(){
+        console.log('ตำบล', this.value);
+    });
+    $('#demo1 [name="amphoe"]').change(function(){
+        console.log('อำเภอ', this.value);
+    });
+    $('#demo1 [name="province"]').change(function(){
+        console.log('จังหวัด', this.value);
+    });
+    $('#demo1 [name="zipcode"]').change(function(){
+        console.log('รหัสไปรษณีย์', this.value);
+    });
+</script>
+</body>
+</html>
